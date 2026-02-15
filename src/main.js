c8370ade9e0ea098e59826d5a0e2bdbafca3bb8e
Vincent Blais <v.blais67@gmail.com>
2026-02-14 21:58:38 -0500

    T

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@
  */
 import WebhookSettings from './views/WebhookSettings.vue'
 import MinimalTest from "./views/MinimalTest.vue";
+import { subscribe } from '@nextcloud/event-bus'
+
+subscribe('spreed:conversation:open', (data) => {
+    console.log('Conversation opened:', data.token);
+    // Logic to inject your settings or UI elements here
+})
+
+console.log(subscribe);
 
 document.addEventListener('DOMContentLoaded', () => {
     if (!window.OCA?.Talk?.Settings?.registerSection) {
